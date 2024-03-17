cb8590a91f1adb900a7720e3c62320f1374f0211
Ahmed Mosalam <74334749+AhmedMosalam1@users.noreply.github.com>
2024-03-17 16:29:42 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,7 @@ const authController = require("../controllers/authController")
 router.get('/getalluser',userController.getAll)
 router.delete('/deleteuser',userController.deleteAll)
 
-router.get("/getuser/:id",userController.getOne)
+router.get("/getuser/:id",userController.setUserIds,userController.getOne)
 router.patch("/updateuser/:id",userController.uploadPhoto,userController.resizePhotoProject,userController.updateOne)
 router.patch("/followunfollowuser/:id",userController.followUnFollowUser)
 router.delete('/deleteuser/:id',userController.deleteOne)
