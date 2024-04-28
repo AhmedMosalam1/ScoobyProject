aacb749e2a93f18bd71c9dcf6ce75854446f7c70
AhmedMosalam1 <Ahmed.mosalam161@gmail.com>
2024-04-29 00:04:59 +0300

    ...

--- routes/communityRoutes.js
+++ routes/communityRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const communityController = require('../controllers/communityController')
 const authController = require("../controllers/authController")
 
-router.use(authController.protect)
+//router.use(authController.protect)
 
 router.post('/addPost/:id',communityController.uploadPhoto1, communityController.resizePhotoProject, communityController.addPost)
 router.patch('/likeAndDisLike/:id',communityController.likeAndDisLike)
