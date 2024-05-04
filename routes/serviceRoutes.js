98c8c36c24ec54b647e9e2e723e9bd7f9d784d86
Ibrahim Esily <89957602+Ibrahimesily@users.noreply.github.com>
2024-05-04 19:11:17 +0300

    Update serviceRoutes.js

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const serviceController = require('../controllers/serviceController')
 
-router.post('/createService/:id',serviceController.uploadPhoto1, serviceController.resizePhotoProject, serviceController.createService)
+router.post('/createService',serviceController.uploadPhoto1, serviceController.resizePhotoProject, serviceController.createService)
 router.get('/getAllServices',serviceController.getAllServices)
 router.get('/getService',serviceController.getService)
 
