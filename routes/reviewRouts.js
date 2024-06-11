ff6e52e7c2d9f23babbe37a7eacb25ae5478f479
rehamsaed <74668745+rehamsaed@users.noreply.github.com>
2024-06-11 12:05:28 +0300

    Update reviewRouts.js

--- routes/reviewRouts.js
+++ routes/reviewRouts.js
@@ -24,7 +24,7 @@ router.get('/getAllReview/:id',reviewController.getAllReview)
 
 router.delete('/deleteReview/:id',reviewController.deleteReview)
 router.patch('/updateReview/:id',reviewController.updateReview)
-router.get('/getMyReviews/:id',reviewController.getMyReviews)
+router.get('/getMyReviews',reviewController.getMyReviews)
 //router.get('/getAllAppReviews',reviewController.getAllAppReviews)
 
 
