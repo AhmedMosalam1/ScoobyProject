0f84259b21e31bd5e6cb31b91dd02e4011a103eb
rehamsaed <74668745+rehamsaed@users.noreply.github.com>
2024-06-11 11:37:32 +0300

    Update petRoutes (add-pet-to-user , get my pets)

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -4,9 +4,9 @@ const user=require('../Models/userModel')
 const petContrller = require('../controllers/petsController')
 const authcontroller=require('../controllers/authController')
 const router = express.Router();
-//router.post('/createplog',plogContrller.uploadPhoto,plogContrller.resizePhotoProject,plogContrller.createPlog)
 
-router.post('/addpet/:id',petContrller.uploadPhoto,petContrller.resizePhotoProject,petContrller.setUserIds,petContrller.addpettouser)
+// router.post('/addpet/:id',petContrller.uploadPhoto,petContrller.resizePhotoProject,petContrller.setUserIds,petContrller.addpettouser)
+router.post('/addpet',petContrller.uploadPhoto,petContrller.resizePhotoProject,authcontroller.protect,petContrller.setUserIds,petContrller.addpettouser)
 router.post('/addpet-shelter/:id',petContrller.uploadPhoto,petContrller.resizePhotoProject,petContrller.setshelterIds,petContrller.addpettoshelter)
 
 router.post('/addpet',petContrller.uploadPhoto,petContrller.resizePhotoProject,petContrller.setUserIds,petContrller.addpet)
@@ -20,9 +20,8 @@ router.get('/filtercatsforkids',petContrller.filtercatsforkids)
 router.get('/successfullyAdaped',petContrller.successAdapted)
 
 
-
 router.get('/getallpets',petContrller.getpets)
-router.get('/getmypets/:id',petContrller.getmypets)
+router.get('/getmypets',authcontroller.protect,petContrller.getmypets)
 router.patch('/updateMyPet/:id',petContrller.uploadPhoto,petContrller.resizePhotoProject,petContrller.updatepet)
 router.delete('/deletePet/:id',petContrller.deletepet)
 
