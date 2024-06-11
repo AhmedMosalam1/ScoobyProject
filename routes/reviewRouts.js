43f3cab77a85e41804a5d25a444921e3a6751aae
rehamsaed <74668745+rehamsaed@users.noreply.github.com>
2024-06-11 11:55:07 +0300

    Update reviewRouts.js

--- routes/reviewRouts.js
+++ routes/reviewRouts.js
@@ -11,13 +11,15 @@ const router=express.Router({mergeParams:true})
 
 router.get('/getAllReview/:id',reviewController.getAllReview)
 
-// router.post('/createReviewService/:id',AuthController.protect,reviewController.setServiceUserIds,reviewController.createReview)
-// router.post('/createReviewDoctor/:id',AuthController.protect,reviewController.setDoctorsUserIds,reviewController.createReview)
+ router.post('/createReviewService/:id',AuthController.protect,reviewController.setServiceUserIds,reviewController.createReviewSerivce)
+ router.post('/createReviewDoctor/:id',AuthController.protect,reviewController.setDoctorsUserIds,reviewController.createReviewDoctor)
+ router.post('/createReviewShelter/:id',AuthController.protect,reviewController.setShelterUserIds,reviewController.createReviewShelter)
 
 
-router.post('/createReviewService/:id',reviewController.createReview)
-router.post('/createReviewDoctor/:id',reviewController.createReview)
-router.post('/createReviewShelter/:id',reviewController.createReview)
+
+// router.post('/createReviewService/:id',reviewController.createReview)
+// router.post('/createReviewDoctor/:id',reviewController.createReview)
+// router.post('/createReviewShelter/:id',reviewController.createReview)
 
 
 router.delete('/deleteReview/:id',reviewController.deleteReview)
