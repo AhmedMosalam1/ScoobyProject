f5f9e1ec0ee877f2a7cd53e7b98a41643b65f857
Ibrahim Esily <89957602+Ibrahimesily@users.noreply.github.com>
2024-06-27 04:08:40 +0300

    Update missing.js

--- AI/missing.js
+++ AI/missing.js
@@ -8,6 +8,7 @@ const sharp = require("sharp");
 const resemble = require("resemblejs");
 
 let image;
+
 //------------------------------------------------------------- upload image
 const multerStorage = multer.memoryStorage();
 
@@ -38,7 +39,7 @@ exports.resizePhotoProject = catchAsync(async (req, res, next) => {
     const filePath = `Scooby/Missing/Missed`;
     const fileName = req.body.petImage;
 
-    const result = await uploadToClodinary(imageBuffer, fileName, filePath);
+    const result = await uploadToCloudinary(imageBuffer, fileName, filePath);
     req.body.petImage = result.secure_url;
     image = req.body.petImage;
 
@@ -48,7 +49,7 @@ exports.resizePhotoProject = catchAsync(async (req, res, next) => {
     next();
 });
 
-const uploadToClodinary = (buffer, filename, folderPath, options = {}) => {
+const uploadToCloudinary = (buffer, filename, folderPath, options = {}) => {
     return new Promise((resolve, reject) => {
         options.folder = folderPath;
         options.public_id = filename;
@@ -65,9 +66,9 @@ const uploadToClodinary = (buffer, filename, folderPath, options = {}) => {
         uploadStream.end(buffer);
     });
 };
+
 //------------------------------------------------------------- classification
 async function getClassificationResult(imageUrl) {
-
     try {
         const response = await axios.get(
             `https://scoopy-ai-api.onrender.com/clssification?image=${encodeURIComponent(
@@ -83,14 +84,12 @@ async function getClassificationResult(imageUrl) {
 //------------------------------------------------------------- similarity
 async function getSimilarityDistance(file1Url, file2Url) {
     try {
-        // تحميل الصور من URLs وتحويلها إلى Buffers
         const response1 = await axios.get(file1Url, { responseType: 'arraybuffer' });
         const response2 = await axios.get(file2Url, { responseType: 'arraybuffer' });
 
         const buffer1 = Buffer.from(response1.data, 'binary');
         const buffer2 = Buffer.from(response2.data, 'binary');
 
-        // استخدام resemble لمقارنة الصور
         return new Promise((resolve, reject) => {
             resemble(buffer1)
                 .compareTo(buffer2)
@@ -103,23 +102,39 @@ async function getSimilarityDistance(file1Url, file2Url) {
         throw new appError("Error in similarity function");
     }
 }
+
 //------------------------------------------------------------- get founded pets
 cloudinary.config({
     cloud_name: "dtny7jzz1",
     api_key: "483257938298228",
     api_secret: "p3M8L2MC4VTpYo0IVXWQV7lf2XA",
 });
+
+// Function to get all resources with pagination
+async function getAllResources(options, resources = []) {
+    try {
+        const result = await cloudinary.api.resources(options);
+        resources = resources.concat(result.resources);
+
+        if (result.next_cursor) {
+            options.next_cursor = result.next_cursor;
+            return await getAllResources(options, resources);
+        } else {
+            return resources;
+        }
+    } catch (error) {
+        throw new appError("Error fetching resources from Cloudinary", 500);
+    }
+}
+
 //------------------------------------------------------------- Missing
 exports.missing = catchAsync(async (req, res, next) => {
     console.log("Start....");
 
-    //Classify the image
     const classificationResult = await getClassificationResult(image);
-    //console.log(classificationResult.Calss);
     console.log(classificationResult.Calss);
     console.log("--------------------");
 
-    // Check classification result
     if (!classificationResult) {
         return next(new appError("Please enter image again", 404));
     } else if (classificationResult.Calss === "other") {
@@ -131,54 +146,48 @@ exports.missing = catchAsync(async (req, res, next) => {
         );
     } else {
         try {
-            //Get founded cats from cloudinary
-            let result;
-            if(classificationResult.Calss === "Cat"){
-                result = await cloudinary.api.resources({
-                    type: "upload",
-                    prefix: "Scooby/Missing/Founded/Cats",
-                });
-            } else if(classificationResult.Calss === "Dog"){
-                result = await cloudinary.api.resources({
-                    type: "upload",
-                    prefix: "Scooby/Missing/Founded/Dogs",
-                });
-            }
+            let options = {
+                type: "upload",
+                prefix: classificationResult.Calss === "Cat" ? "Scooby/Missing/Founded/Cats" : "Scooby/Missing/Founded/Dogs",
+            };
 
-            const foundedPets = result.resources.map((obj) => obj.secure_url);
+            const resources = await getAllResources(options);
+            const foundedPets = resources.map((obj) => obj.secure_url);
 
             console.log(foundedPets);
             console.log("--------------------");
 
-            // Calculate similarity for each founded pet
-            let similarityArray = [];
+            const similarityPromises = foundedPets.map(async (foundedPet) => {
+                const similarity = await getSimilarityDistance(image, foundedPet);
 
-            for (let i = 0; i < foundedPets.length; i++) {
-                let similarity = await getSimilarityDistance(image, foundedPets[i]);
+                const user = await FoundedModel.find({ petImage: foundedPet }).populate('userId');
+                const userId = user[0].userId;
+                const description = user[0].description;
+                const location = user[0].locations;
+                const phoneNumber = user[0].phoneNumber;
+                const createdAt = user[0].createdAt;
 
-                let user = await FoundedModel.find({ petImage: foundedPets[i] }).populate('userId');
-                let userId = user[0].userId;
-                let description = user[0].description;
-                let location = user[0].locations;
-                let phoneNumber = user[0].phoneNumber;
-                let createdAt = user[0].createdAt;
-                console.log(similarity);
-                console.log(user[0]);
-                console.log("--------------------");
-                similarityArray.push({
+                return {
                     similarity: 100 - similarity.rawMisMatchPercentage,
-                    url: foundedPets[i],
+                    url: foundedPet,
                     description,
                     userId,
                     location,
                     phoneNumber,
                     createdAt
-                });
-            }
+                };
+            });
+
+            const similarityArray = await Promise.all(similarityPromises);
+
             let sortedSimilarityArray = similarityArray.sort((a, b) => b.similarity - a.similarity);
             console.log(sortedSimilarityArray);
             console.log("--------------------");
-            res.status(200).json({ uploadedImage: image,similarityArray: sortedSimilarityArray.slice(0, 2) });
+
+            res.status(200).json({ 
+                uploadedImage: image, 
+                similarityArray: sortedSimilarityArray.slice(0, 2),
+            });
             console.log("End......");
         } catch (err) {
             return next(new appError("Error in processing the request", 500));
